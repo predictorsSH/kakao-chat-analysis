743842048564e46bf16594404327fb9347853769
sanghyun <sanghyun9082@gmail.com>
2022-09-20 23:19:19 +0900

    data read, user count 간이 구현

---
+++ whorwe/core/functions/read_data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+FILE_PATH = '../../../media/uploads/KakaoTalk_Chat.csv'
+
+def data_read(file_path):
+    chat_data = pd.read_csv(file_path)
+    print(chat_data.info())
+    return chat_data
+
+if __name__ == '__main__':
+    chat_data = data_read(FILE_PATH)
+    chat_data.head(3)
