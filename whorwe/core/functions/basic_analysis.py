466e9a13f98193472c8a706b65a70dd18c967f7b
sanghyun <sanghyun9082@gmail.com>
2022-10-02 21:50:56 +0900

    Word count 함수 구현

--- whorwe/core/functions/basic_analysis.py
+++ whorwe/core/functions/basic_analysis.py
@@ -1,8 +1,27 @@
 from read_data import data_read
 
+#사용자
+def user(data) -> list :
+    users = data['User'].unique()
+    return users
+
+# 말 많은 사람 찾기
 def user_counts(data):
     u_counts = data['User'].value_counts()
     return u_counts
+
+def words_counts(user,data):
+    data = data[data['User'] == user]['morpheme']
+
+    counts = {}
+    print('counting words by ', user, '...')
+    for d in data:
+        for word in d:
+            print(word)
+            counts[word] = counts.get(word, 0) + 1
+
+    return sorted(counts.items(), key=lambda x: x[1], reverse=True)
+
 
 
 if __name__ =='__main__':
--- whorwe/core/preprocess/preprocessing.py
+++ whorwe/core/preprocess/preprocessing.py
@@ -1,6 +1,7 @@
 
 from konlpy.tag import Okt
 from read_data import data_read
+from basic_analysis import words_counts
 
 def _tokenizer(message: str):
     okt = Okt()
@@ -18,6 +19,7 @@ if __name__ == '__main__':
     FILE_PATH = '../../../media/uploads/KakaoTalk_Chat.csv'
     chat_data = data_read(FILE_PATH)
     chat_data = make_morpheme(chat_data)
-    
+    words = words_counts('김상현',chat_data)
+
 
 
