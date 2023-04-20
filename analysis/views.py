d4c7964e5ff66e54029ed68795bcae2800f90257
predictorsSH <sanghyun9082@gmail.com>
2023-04-20 10:50:20 +0900

    file upload react django 연동

--- analysis/views.py
+++ analysis/views.py
@@ -11,52 +11,60 @@ from whorwe.mnps.mnps import DataProcess
 import json
 
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+import csv
+import io
 
+@method_decorator(csrf_exempt, name="dispatch")
 def fileUploadView(request):
 
 
     if request.method == "POST":
 
-        attached = request.FILES
+        attached = request.FILES['file']
+        ## 바로 읽어오는게 더 좋을수도
+        # decoded_file = attached.read().decode('utf-8')
+        # io_string = io.StringIO(decoded_file)
+        #
+        # data = list()
+        # for line in csv.reader(io_string, delimiter=','):
+        #     print(line)
         fileupload = FileUpload(
             attached=attached
         )
         fileupload.save()
 
-        # 데이터 전처리
-    #     dp = DataProcess(fileupload.attached.path)
-    #     # user(채팅 참가자)별로 말한 횟수 카운트
-    #     u_count, act_time, u_words_count = dp.basic_analysis()
-    #     #user별 count dictionary 생성
-    #     u_count_data = {}
-    #     for n, c in u_count.items():
-    #         u_count_data[n] = c
-    #
-    #     #Basic_stats에 count 입력하고 저장.
-    #     basic_stats = Basic_stats(
-    #         user_count=json.dumps(u_count_data, ensure_ascii=False),
-    #         active_time=act_time,
-    #         user_words_count=json.dumps(u_words_count, ensure_ascii=False)
-    #     )
-    #     basic_stats.save()
-    #
-    #     advanced_analysis = Advanced_analyis(
-    #         test='테스트 개발'
-    #     )
-    #     advanced_analysis.save()
-    #
-    #     return redirect('fileupload')
-    #
-    # else:
-    #
-    #     fileuploadForm = forms.FileUploadForm
-    #     context = {
-    #         'fileuploadForm': fileuploadForm
-    #     }
+        #데이터 전처리
+        dp = DataProcess(fileupload.attached.path)
+        # user(채팅 참가자)별로 말한 횟수 카운트
+        u_count, act_time, u_words_count = dp.basic_analysis()
+        #user별 count dictionary 생성
+        u_count_data = {}
+        for n, c in u_count.items():
+            u_count_data[n] = c
 
+        #Basic_stats에 count 입력하고 저장.
+        basic_stats = Basic_stats(
+            user_count=json.dumps(u_count_data, ensure_ascii=False),
+            active_time=act_time,
+            user_words_count=json.dumps(u_words_count, ensure_ascii=False)
+        )
+        basic_stats.save()
 
-    # return render(request, "analysis/fileupload.html", context)
-    return HttpResponse(json.dumps({"status":"Success"}))
+        advanced_analysis = Advanced_analyis(
+            test='테스트 개발'
+        )
+        advanced_analysis.save()
+
+        return redirect('fileupload')
+    else:
+        fileuploadForm = forms.FileUploadForm
+        context = {
+            'fileuploadForm': fileuploadForm
+        }
+    return render(request, "analysis/fileupload.html", context)
+
 
 class BasicStatView(APIView):
     def get(self, request, id):
