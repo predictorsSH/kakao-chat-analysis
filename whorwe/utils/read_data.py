49b5556e0c161bf747f4e8194451300b7c1a9477
sanghyun <sanghyun9082@gmail.com>
2023-06-20 23:54:15 +0900

    'txt file read 수정'

--- whorwe/utils/read_data.py
+++ whorwe/utils/read_data.py
@@ -10,43 +10,64 @@ def data_read(file_path):
 
 def txt_read(file_path):
 
-    pattern = r"--------------- \d{4}년 \d{1,2}월 \d{1,2}일 \w요일 ---------------"
-    data = []
+
+    pattern = r'-----+ (\d+)년 (\d+)월 (\d+)일 [월화수목금토일]요일 ----+'
+    chat_data = []
 
     with open(file_path, 'r') as f:
-        for line in f.readlines():
-            match = re.match(line, pattern)
+        f_data = f.readlines()
+        match = re.fullmatch(pattern, f_data[3].strip())
+        date = [match.group(1), match.group(2), match.group(3)]
+        date = '-'.join(date)
+
+        for line in f_data[4:]:
+            line = line.strip()
+            print(line)
+
+
+            try:
+                match = re.fullmatch(pattern, line)
+                print(match)
+            except re.error as e:
+                print("정규식 오류로 스킵, e")
 
             if match:
+
                 date = [match.group(1), match.group(2), match.group(3)]
                 date = '-'.join(date)
 
             else:
-                user_start = line.find("[") + 1
-                user_end = line.find("]")
-                user = line[user_start:user_end]
 
-                # 시간 추출
-                time_start = line.find("[", user_end + 1) + 1
-                time_end = line.find("]", time_start)
-                time = line[time_start:time_end]
-                period = time.split(' ')[0]
-                time = time.split(' ')[1]
+                try:
+                    user_start = line.find("[") + 1
+                    user_end = line.find("]")
+                    user = line[user_start:user_end]
 
-                if period == '오후':
-                    time = [str(int(time.split(':')[0]) + 12), time.split(':')[1]]
-                    time = ':'.join(time)
+                    # 시간 추출
+                    time_start = line.find("[", user_end + 1) + 1
+                    time_end = line.find("]", time_start)
+                    time = line[time_start:time_end]
+                    print(time)
+                    period = time.split(' ')[0]
+                    time = time.split(' ')[1]
 
-                else:
-                    time = time
+                    if period == '오후':
+                        time = [str(int(time.split(':')[0]) + 12), time.split(':')[1]]
+                        time = ':'.join(time)
 
-                # Message
-                message_start = line.find(" ", time_end + 1) + 1
-                message = line[message_start:]
+                    else:
+                        time = time
 
-            data.append({'Date': date+' '+time, 'User': user, 'Message': message})
+                    # Message
+                    message_start = line.find(" ", time_end + 1) + 1
+                    message = line[message_start:]
 
-    return data
+                except:
+                    continue
+
+            chat_data.append({'Date': date + ' ' + time, 'User': user, 'Message': message})
+
+    return chat_data
 
 
 
